d50d7ee9afd68a65410336a23cd5d654b934aaff
Dexcop <ardellejodynathaniel@gmail.com>
2024-05-28 17:33:05 +0700

    routing

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- <script src="/js/main.js">  -->
+    <script>
+        function goTo() {
+            window.location.replace("html/home.php");
+        }
+    </script>
 </head>
-<body>
-    
+
+<body onload="goTo()">
 </body>
-<script src="/js/main.js"></script>
+
 </html>
