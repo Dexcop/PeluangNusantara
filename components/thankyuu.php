d4445e2b366d1f9214026026b89f6ffa08354fc1
Rnvz <yohaneswenanta2410@gmail.com>
2024-05-29 17:49:15 +0700

    niga

---
+++ components/thankyuu.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../css/thankyou.css">
+</head>
+    <body>
+        <div class="container">
+            <h1>Thank You!</h1>
+                <p>Your submission has been received.</p>
+            <button onclick="window.location.href='index.php'">Go Home</button>
+        </div>
+    </body>
+</html>
