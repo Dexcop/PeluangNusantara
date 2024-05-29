91fe8a49ae19448ac7396cb36afe8a68cb07bf5a
Dexcop <ardellejodynathaniel@gmail.com>
2024-05-29 14:52:36 +0700

    navbar n home

--- components/navbar.php
+++ components/navbar.php
@@ -1,13 +1,13 @@
-  <link rel="stylesheet" href="../css/navbar.css">
-  <header>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
-    <div class="mylogo"><img src="../assets/logo.png" alt=""></div>
-    <a href="../html/register.php" class="joinuss"><img src="../assets/join-us.png" alt=""></a>
-  </header>
+<link rel="stylesheet" href="../css/navbar.css">
+<header>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  <div class="mylogo"><img src="../assets/logo.png" alt=""></div>
+  <a href="../html/register.php" class="joinuss"><img src="../assets/join-us.png" alt=""></a>
+</header>
--- html/information.php
+++ html/information.php
@@ -1,17 +1,192 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Peluang Nusantara - Information</title>
-        <!-- <link rel="stylesheet" href="/css/Information.css"> -->
-      </head>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peluang Nusantara - Information</title>
+    <link rel="stylesheet" href="../css/information.css">
+</head>
+
 <body>
-    <?php include '../components/navbar.php'?>
-      
-    <h1>
-        Leo
-    </h1>
-    <?php include '../components/footer-bawah.php'?>
+
+    <div class="container">
+        <div class="left">
+            <h1 class="title">Water for africa</h1>
+            <img src="../assets/desert.jpg"
+                class="image" alt="Water for africa">
+            <div class="content">
+                <p>In 2018, a group of volunteering students launched a charitable campaign entitled @Ele_Elna_Elak,
+                    which means in Arabic, "What we own, you own..." in a clear message to our belief in social
+                    solidarity and that we must all provide assistance to those in need... through... For the past six
+                    years, we have been providing food, warm clothes and blankets in the winter to thousands of families
+                    in need.</p>
+                <p>During this war, and our families were subjected to displacement, bombing, and starvation, as
+                    happened to all people of the Gaza Strip, but we decided to continue helping families in need under
+                    these dire circumstances, to obtain</p>
+                <p><a href="#">Read more</a></p>
+            </div>
+            <div class="update">
+                <h2>Update</h2>
+                <p>20/03/2024 anonymous</p>
+                <p>YOU MADE THIS HAPPEN!:)</p>
+                <p>We have increased our goal, as water appears to be the first and most needed essential in Africa now,
+                    especially during the blockade!</p>
+                <p>Thanks for your efforts, support and collaboration, people of Africa thank you a lot! Shukran!</p>
+                <p><a href="#">Read more</a></p>
+            </div>
+            <div class="organizer">..
+                <h2>Organizer</h2>
+                <div class="organizer-info">
+                    <div class="person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p>Sirye letme yermen</p>
+                        <p>organization 19/01/2020</p>
+                    </div>
+                </div>
+                <button class="contact">Contact</button>
+            </div>
+            <div class="words-of-support">
+                <h2>Words of Support</h2>
+                <p>Please donate to share words of support.</p>
+                <div class="support-item">
+                    <div class="support-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p class="support-text">Amira dos ques</p>
+                        <p class="support-text">organization 19/01/2020</p>
+                        <p>Funds raised by Holymarket vendors and partecipants on the event of the 5th of May-Lisbon.
+                            Supporting the people of africal</p>
+                    </div>
+                </div>
+                <div class="support-item">
+                    <div class="support-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p class="support-text">Amira dos ques</p>
+                        <p class="support-text">organization 19/01/2020</p>
+                        <p>Funds raised by Holymarket vendors and partecipants on the event of the 5th of May-Lisbon.
+                            Supporting the people of africaf</p>
+                    </div>
+                </div>
+                <div class="support-item">
+                    <div class="support-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p class="support-text">Amira dos ques</p>
+                        <p class="support-text">organization 19/01/2020</p>
+                        <p>Funds raised by Holymarket vendors and partecipants on the event of the 5th of May-Lisbor.
+                            Supporting the people of africa</p>
+                    </div>
+                </div>
+                <div class="support-item">
+                    <div class="support-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p class="support-text">Amira dos ques</p>
+                        <p class="support-text">organization 19/01/2020</p>
+                        <p>Funds raised by Holymarket vendors and partecipants on the event of the 5th of May-Lisbon.
+                            Supporting the people of africal</p>
+                    </div>
+                </div>
+                <button class="show-more">Show More</button>
+            </div>
+            <div class="footer">
+                <p>Created January 19st, 2020</p>
+                <p><a href="#">Report fundraiser</a> | <a href="#">Emergencies</a></p>
+            </div>
+        </div>
+        <div class="right">
+            <h1 class="title">$123,456</h1>
+            <p>12.34K donation</p>
+            <div class="donation-bar">
+                <div class="progress"></div>
+            </div>
+            <button class="donate-button">Donate</button>
+            <div class="donations">
+                <div class="donation-item">
+                    <div class="donation-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p>Anonymous</p>
+                        <p class="donation-amount">$123</p>
+                        <p>01/12/2023</p>
+                    </div>
+                </div>
+                <div class="donation-item">
+                    <div class="donation-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p>Anonymous</p>
+                        <p class="donation-amount">$123</p>
+                        <p>01/12/2023</p>
+                    </div>
+                </div>
+                <div class="donation-item">
+                    <div class="donation-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p>Anonymous</p>
+                        <p class="donation-amount">$123</p>
+                        <p>01/12/2023</p>
+                    </div>
+                </div>
+                <div class="donation-item">
+                    <div class="donation-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p>Anonymous</p>
+                        <p class="donation-amount">$123</p>
+                        <p>01/12/2023</p>
+                    </div>
+                </div>
+                <div class="donation-item">
+                    <div class="donation-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p>Anonymous</p>
+                        <p class="donation-amount">$123</p>
+                        <p>01/12/2023</p>
+                    </div>
+                </div>
+                <div class="donation-item">
+                    <div class="donation-person">
+                        <img src="../assets/people-01.png"
+                            alt="Person">
+                    </div>
+                    <div>
+                        <p>Anonymous</p>
+                        <p class="donation-amount">$123</p>
+                        <p>01/12/2023</p>
+                    </div>
+                </div>
+                <button class="see-all-button">See All</button>
+                <button class="see-top-button">See Top</button>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
