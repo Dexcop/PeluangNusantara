527985707966dc996aeef32abea187981aca0a9d
Dexcop <ardellejodynathaniel@gmail.com>
2024-05-29 20:50:29 +0700

    failed to do popup for action form

--- components/thankyuu.php
+++ components/thankyuu.php
@@ -1,10 +1,51 @@
-<link rel="stylesheet" href="../css/thankyou.css">
-</head>
-    <body>
-        <div class="container">
-            <h1>Thank You!</h1>
-                <p>Your submission has been received.</p>
-            <button onclick="window.location.href='index.php'">Go Home</button>
-        </div>
-    </body>
-</html>
+<!-- <link rel="stylesheet" href="../css/thankyou.css"> -->
+
+<div class="container" id="thanksModal">
+    <style>
+        body {
+            font-family: sans-serif;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 50px auto;
+            background-color: #fff;
+            padding: 30px;
+            border-radius: 5px;
+            outline: 2px solid black;
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 3em;
+            margin-bottom: 0.5em;
+        }
+
+        p {
+            font-size: 1.2em;
+            margin-bottom: 1em;
+        }
+
+        button {
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            padding: 1em 2em;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 1.2em;
+            border-radius: 20px;
+            cursor: pointer;
+        }
+    </style>
+    <h1>Thank You!</h1>
+    <p>Your submission has been received.</p>
+    <button onclick="window.location.href='../html/home.php'">Go Home</button>
+</div>
