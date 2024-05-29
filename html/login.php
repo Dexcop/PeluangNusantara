c3dfa634560724fa8fe9517eb2cfb1936e5914a5
Dexcop <ardellejodynathaniel@gmail.com>
2024-05-29 12:33:59 +0700

    login

--- html/login.php
+++ html/login.php
@@ -5,48 +5,29 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Peluang Nusantara - Login</title>
-  <!-- <link rel="stylesheet" href="/css/register.css"> -->
+  <link rel="stylesheet" href="../css/register.css">
 </head>
 
 <body>
-  <?php include '../components/navbar.php'?>
+  <?php include '../components/navbar.php' ?>
 
   <div class="content">
-    <link href="./c_sign-in.css" rel="stylesheet" />
-    <div class="c_sign-in-frame">
-      <!-- <img src="./assets/2192.png" alt="rectangle" width="216" height="92.23193359375" class="c_sign-in-rectangle" /> -->
-      <div class="c_sign-in-text10">
-        <p class="c_sign-in-text11">Belum punya akun?</p>
-      </div>
-      <div class="c_sign-in-text12">
-        <a href="/html/register.html">Daftar</a>
-      </div>
-      <div class="c_sign-in-text14">
-      </div>
-      <div class="c_sign-in-text">
-        <h1 class="c_sign-in-text01">Selamat Datang</h1>
-      </div>
-      <div class="c_sign-in-text02">
-        <input type="text" name="email" placeholder="E-mail Address">
-      </div>
-      <br>
-      <div class="c_sign-in-text04">
-        <input type="text" name="password" placeholder="Password">
-      </div>
-      <div class="c_sign-in-text06">
-        <a href="" ><strong>Lupa Password?</strong></a>
-      </div>
-      <div class="c_sign-in-text08">
-        <p class="c_sign-in-text09">
-          Dengan mengklik tombol Masuk di bawah, Anda menyetujui Ketentuan Layanan
-          Peluang Nusantara dan menyetujui Pemberitahuan Privasi.
-        </p>
-      </div>
-      
-    </div>
-  </div>
+    <h1>Welcome Back!</h1>
+    <form action="/submit_registration" method="post">
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email" required>
 
-  <?php include '../components/footer-bawah.php'?>
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" required>
+
+      <div class="password-link">
+        <a href="../html/login.php">Forgot Password</a>
+      </div>
+
+      <button type="submit">Login</button>
+    </form>
+
+  </div>
 </body>
 
 </html>
