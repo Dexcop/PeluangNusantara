3c6d7e5acf4b08baeb0f0dc336fdd492ba666e32
leonardvanderson <leonard.vanderson@gmail.com>
2024-05-29 16:38:35 +0700

    ok

---
+++ html/donationinti.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Donation Inti</title>
+    <link rel = "stylesheet" href = "../css/donationinti.css">
+</head>
+<body>
+<?php include '../components/navbar.php'?>
+<h1>Medical</h1>
+    <div class="section">
+        <img src="https://www.vergara.law/immigration/removal-deportation-defense/" alt="Cancer Support for Ethann">
+        <p>Medical Support</p>
+        <p>Cancer Support for Ethann</p>
+        <h5>Lorem Ipsum Dolor sit amet</h5>
+        <button type="submit">Learn more</button>
+    </div>
+<head>
+    <title>Water for Africa</title>
+</head>
+<body>
+    <img src="https://i.imgur.com/F9g612k.jpg" alt="Tree in a desert landscape">
+    <h1>Water for Africa</h1>
+    <progress value="1234567" max="10000000">
+    </progress>
+    <p>$1,234,567 Raised</p>
+</body>
+<?php include '../components/footer-atass.php'?>
+<?php include '../components/footer-bawah.php'?>
+</body>
+</html>
---
+++ html/donationmenu.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Donation Menu</title>
+    <link rel = "stylesheet" href = "../css/donationmenu.css">
+</head>
+<body>
+    <?php include '../components/navbar.php'?>
+    <h1>Fundraiser categories</h1>
+    <div class="grid-container">
+  <div class="grid-item">
+    <img src="https://i.imgur.com/l2uK05x.jpg" alt="Medical">
+    <p>Medical</p>
+  </div>
+  <div class="grid-item">
+    <img src="https://i.imgur.com/1Dq2t0M.jpg" alt="Memorial">
+    <p>Memorial</p>
+  </div>
+  <div class="grid-item">
+    <img src="https://i.imgur.com/5W7n5Qj.jpg" alt="Emergency">
+    <p>Emergency</p>
+  </div>
+  <div class="grid-item">
+    <img src="https://i.imgur.com/2W4L2oW.jpg" alt="Non Profit">
+    <p>Non Profit</p>
+  </div>
+  <div class="grid-item">
+    <img src="https://i.imgur.com/wT61q2d.jpg" alt="Financial Emergency">
+    <p>Financial Emergency</p>
+  </div>
+  <div class="grid-item">
+    <img src="https://i.imgur.com/y648w6A.jpg" alt="Animal">
+    <p>Animal</p>
+  </div>
+</div>
+<div class="container">
+  <div class="section">
+    <img src="https://www.pinterest.com/pin/305963368438041271/" alt="Books for SEA">
+    <p>Education</p>
+    <p>Books for SEA</p>
+    <h5>Lorem Ipsum Dolor sit amet</h5>
+  </div>
+  <div class="section">
+    <img src="https://www.vergara.law/wp-content/uploads/2020/11/immigration.png" alt="Cancer Support for Ethann">
+    <p>Medical Support</p>
+    <p>Cancer Support for Ethann</p>
+    <h5>Lorem Ipsum Dolor sit amet</h5>
+  </div>
+  <div class="section">
+    <img src="https://i0.wp.com/zenkittyworld.com/wp-content/uploads/2023/12/pexels-engin-akyurt-14096854.jpg?w=1280&ssl=1" alt="one eye cat rescue">
+    <p>Animal</p>
+    <p>one eye cat rescue</p>
+    <h5>Lorem Ipsum Dolor sit amet</h5>
+  </div>
+</div>
+    <?php include '../components/footer-atass.php'?>
+    <?php include '../components/footer-bawah.php'?>
+</body>
+</html>
