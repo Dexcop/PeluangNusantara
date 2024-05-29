0be768f82f43f25c0cc088c4518c58ec68278e52
leonardvanderson <leonard.vanderson@gmail.com>
2024-05-29 17:36:01 +0700

    fix:title

--- html/information.php
+++ html/information.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Peluang Nusantara - Information</title>
+    <title>Peluang Nusantara - Information system</title>
     <link rel="stylesheet" href="../css/information.css">
 </head>
 
