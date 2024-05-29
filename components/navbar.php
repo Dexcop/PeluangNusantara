52f036ff0d61d4dd23aefdf8f22ca68fe501973b
Dexcop <ardellejodynathaniel@gmail.com>
2024-05-29 15:23:32 +0700

    navbar

--- components/navbar.php
+++ components/navbar.php
@@ -3,9 +3,9 @@
   <nav>
     <ul>
       <li><a href="../html/home.php">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><a href="#">Donate</a></li>
+      <li><a href="#">About Us</a></li>
+      <!-- <li><a href="#">Contact</a></li> -->
     </ul>
   </nav>
   <div class="mylogo"><img src="../assets/logo.png" alt=""></div>
